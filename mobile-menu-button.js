06d2c3a8c8968cb9473be709de1d373dba0d673c
ArturCodes <arturshapiro@gmail.com>
2023-04-19 08:48:47 -0500

    .

--- mobile-menu-button.js
+++ mobile-menu-button.js
@@ -1,9 +1,18 @@
 function show() {
+    var bar1 = getElementByClass('bar1')
+    var bar2 = getElementByClass('bar2')
+    var bar3 = getElementByClass('bar3')
+
+
     document.getElementById('opacity-bg').classList.replace('hide', 'show');
-    document.getElementById('mobile-navbar-container').classList.replace('hide', 'show');
+    document.getElementById('navbar-menu').classList.replace('hide', 'show');
 }
 
 function hide() {
     document.getElementById('opacity-bg').classList.replace('show', 'hide');
-    document.getElementById('mobile-navbar-container').classList.replace('show', 'hide');
+    document.getElementById('navbar-menu').classList.replace('show', 'hide');
+}
+
+function menuBars() {
+    
 }
