dca570db98236066d3afaaf18f92c8a8b1b53cb1
ArturCodes <arturshapiro@gmail.com>
2023-05-20 22:47:29 -0500

    fixed

--- mobile-menu-button.js
+++ mobile-menu-button.js
@@ -1,9 +1,11 @@
 function show() {
     document.getElementById('navbar-links').style.width = "240px"
     document.getElementById('opacity-bg').style.display = "block"
+    document.getElementById('mobile-menu-close').style.display = "block"
 }
 
 function hide() {
     document.getElementById('navbar-links').style.width = "0px"
     document.getElementById('opacity-bg').style.display = "none"
+    document.getElementById('mobile-menu-close').style.display = "none"
 }
